6e42b65677fbed93469c5226da882338f990f522
DESKTOP-USAIJ42\川田 <fidelioman.04@gmail.com>
2017-12-27 21:19:59 +0900

    演習1-1をコミット

---
+++ src/Ensyu1of1.java
@@ -0,0 +1,18 @@
+
+
+public class Ensyu1of1 {
+/**
+ *  プログラム中の文の終端を示すセミコロン;が欠如しているとどうなるか。
+ *  プログラムをコンパイルして検証せよ。
+ * @param args
+ */
+	//mainメソッドの宣言。
+	public static void main(String[] args){
+		//初めてのJavaプログラム。を出力して。
+		System.out.println("初めてのJavaプログラム。");
+		//画面に出力しています。を出力して改行。
+		System.out.println("画面に出力しています。");
+		// ""を出力して改行
+		System.out.println("")
+	}
+}
---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args){
+		System.out.println("初めてのJavaプログラム。");
+		System.out.println("画面に出力しています。");
+		System.out.println("");
+
+	}
+}
