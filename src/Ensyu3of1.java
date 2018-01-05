a87c60447490901387f7fcab1b3622f609526b24
DESKTOP-USAIJ42\川田 <fidelioman.04@gmail.com>
2018-01-05 21:29:04 +0900

    演習1の3コミット

---
+++ src/Ensyu3of1.java
@@ -0,0 +1,14 @@
+
+public class Ensyu3of1 {
+/**
+ * 右に示すように、各行に1文字ずつ名前を表示するプログラムを作成せよ。
+ * 姓と名のあいだは1行あけることとし、自分の名前を表示すること。
+ * @param args
+ */
+	//mainメソッドの宣言。
+	public static void main(String[]args){
+		//川田 大史を改行して出力する。
+		System.out.print("川\n田\n\n大\n史");
+
+	}
+}
