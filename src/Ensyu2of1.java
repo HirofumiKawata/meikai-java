92c07857a8efbb3a4f631d59bb61b5e04db09e75
DESKTOP-USAIJ42\川田 <fidelioman.04@gmail.com>
2018-01-10 20:54:44 +0900

    演習2-1です。ご確認をよろしくお願いします。

---
+++ src/Ensyu2of1.java
@@ -0,0 +1,13 @@
+
+public class Ensyu2of1 {
+/**
+ * 右に示すように、各行に1文字ずつ名前を表示するプログラムを作成せよ。
+ * 著者の名前ではなく、自分の名前を表示すること。
+ * @param args
+ */
+	//mainメソッドの宣言。
+	public static void main(String[]args){
+		//川田大史を出力して。
+		System.out.print("川\n田\n大\n史");
+	}
+}
